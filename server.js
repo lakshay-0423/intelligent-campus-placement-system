36dd2571f23ad53d8a8a1040cbe62e5daea5abf0
lakshay-0423 <blakshay162@gmail.com>
2026-02-09 11:58:35 +0530

    add basic routing and health/info APIs

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    res.end("Intelligent Campus Placement System");
+    if(req.url=== '/health' && req.method==='GET'){
+        res.writeHead(200,{"content-type" : "text/plain"});
+        res.end("ok");
+    }
+    else if(req.url === '/info' && req.method === "GET"){
+        res.writeHead(200,{"content-type" : "application/json"});
+        res.end(JSON.stringify({project : "Intelligent Campus Placement System"}));
+    }
+    else{
+        res.writeHead(404);
+        res.end("Not Found");
+    }
 });
 
 server.listen(3000,()=>{
