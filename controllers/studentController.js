da13f770b5414e66d99c7280923bc991329c6fd7
lakshay-0423 <blakshay162@gmail.com>
2026-02-19 14:37:16 +0530

    add PUT and DELETE APIs with ID validation for students

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -21,3 +21,37 @@ exports.createStudents = (req,res) => {
 
     res.status(201).json({message : "Student Created"});
 };
+
+exports.updateStudent = (req, res) => {
+  const id = parseInt(req.params.id);
+  const students = JSON.parse(
+    fs.readFileSync("./data/students.json")
+  );
+
+  const index = students.findIndex(s => s.id === id);
+
+  students[index] = { ...students[index], ...req.body };
+
+  fs.writeFileSync(
+    "./data/students.json",
+    JSON.stringify(students, null, 2)
+  );
+
+  res.json({ message: "Student updated" });
+};
+
+exports.deleteStudent = (req, res) => {
+  const id = parseInt(req.params.id);
+  let students = JSON.parse(
+    fs.readFileSync("./data/students.json")
+  );
+
+  students = students.filter(s => s.id !== id);
+
+  fs.writeFileSync(
+    "./data/students.json",
+    JSON.stringify(students, null, 2)
+  );
+
+  res.json({ message: "Student deleted" });
+};
--- routes/students.js
+++ routes/students.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const studentController = require("../controllers/studentController");
+const validateId = require("../middleware/validateId");
 
 router.get('/', studentController.getStudents);
+
 router.post('/',studentController.createStudents);
+
+router.put(
+  "/:id",
+  validateId("./data/students.json"),
+  studentController.updateStudent
+);
+
+router.delete(
+  "/:id",
+  validateId("./data/students.json"),
+  studentController.deleteStudent
+);
 
 module.exports = router;
